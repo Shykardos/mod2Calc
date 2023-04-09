7bfe356a890bd0bd829e13d8ab8c200b1b817087
volodymyr <vova.shunkarenko@gmail.com>
2023-04-09 23:31:12 +0300

    add SumCalculator and test

--- src/main/java/com/goit/SumCalculator.java
+++ src/main/java/com/goit/SumCalculator.java
@@ -1,2 +1,20 @@
-package com.goit;public class SumCalculator {
+package com.goit;
+
+public class SumCalculator {
+
+    public int sum(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("n must be bigger than 1");
+        }
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
+
--- src/test/java/com/goit/SumCalculatorTest.java
+++ src/test/java/com/goit/SumCalculatorTest.java
@@ -1,4 +1,38 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.goit;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class SumCalculatorTest {
-  
+    private SumCalculator sumCalculator;
+
+    @BeforeEach
+    void setUp() {
+        sumCalculator = new SumCalculator();
+    }
+
+    @Test
+    void testSumWithOne() {
+        int value = 1;
+        int actual = sumCalculator.sum(1);
+        assertEquals(value, actual);
+        System.out.println(actual);
+    }
+
+    @Test
+    void testSumWithThree() {
+        int value = 6;
+        int actual = sumCalculator.sum(3);
+        assertEquals(value, actual);
+    }
+
+    @Test
+    void testSumWithZero() {
+        assertThrows(IllegalArgumentException.class, () -> sumCalculator.sum(0));
+    }
+
 }
